c499d478df2592f62b8eb39b7738c3425b1797c2
Dhaman677 <Dhaman677@users.noreply.github.com>
2025-10-28 09:53:24 +0000

    deploy: d2fc494fc1936a485c353b1679598dc1e2645b51

--- EXP4/EXP4.js
+++
@@ -1,6 +0,0 @@
-const textInput = document.getElementById("textInput");
-const charCount = document.getElementById("charCount");
-
-textInput.addEventListener("input", function () {
-  charCount.innerText = textInput.value.length;
-});
